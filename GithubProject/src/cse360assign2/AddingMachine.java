ba40ffda2ef2fb054f040aa63036d5cb0ad4ee5b
Ethan <Ethan@BB-8>
2019-10-08 10:03:40 -0700

    Initial Commit of Project

---
+++ GithubProject/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,44 @@
+package cse360assign2;
+
+/**
+ * This Adding Machine class holds a total, and supports methods to add and subtract from it
+ * 
+ * @author Ethan H. M. Berglund
+ * @version 1.0
+ * @since 10-8-2019
+ *
+ */
+public class AddingMachine {
+
+	private int total;
+	
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	
+	public void add (int value) {
+		
+	}
+	
+	
+	public void subtract (int value) {
+		
+	}
+		
+	
+	public String toString () {
+		return "";
+	}
+
+	
+	public void clear() {
+	
+	}
+}
